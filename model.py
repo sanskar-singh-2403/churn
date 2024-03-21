25d37d94d2198f8c0ed2b697e69cb627e970fd0c
Ken Kaneki <sanskarsinghty1234@gmail.com>
2024-03-21 15:37:20 +0530

    Final tuning

--- model.py
+++ model.py
@@ -1,19 +1,18 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import classification_report
+import pandas as pd
 
-# Split the data
+df = pd.read_csv("./churn.csv");
+
 X = df.drop('target', axis=1)
 y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train the model
 gb_model = GradientBoostingClassifier()
 gb_model.fit(X_train, y_train)
 
-# Evaluate the model
 y_pred = gb_model.predict(X_test)
 print(classification_report(y_test, y_pred))
 
-# Predict churn probabilities
 probabilities = gb_model.predict_proba(X_test)
